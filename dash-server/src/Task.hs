b77f31521a4a64e0abc29cc4e3f903b339e48d59
Benedikt Friedrich <friedri6@hm.edu>
2018-01-09 15:56:56 +0100

    sort tasks returns key list

--- dash-server/src/Task.hs
+++ dash-server/src/Task.hs
@@ -6,8 +6,11 @@
 module Task
   ( TaskAPI
   , taskServer
+  , concatDep
+  , sortDep
   ) where
 
+import           Data.Monoid
 import           Data.String
 import           Database.Persist.Sql
 import           Entity
@@ -16,10 +19,12 @@ import           Types
 
 type TaskAPI = "task" :>
   (    "add" :> ReqBody '[JSON] Task :> Get '[JSON] [Dependency]
+  :<|> "sort" :> Get '[JSON] [Int]
   )
 
 taskServer :: ConnectionPool -> Server TaskAPI
 taskServer pool = addTask
+             :<|> sort
   where
     addTask :: Task -> Handler [Dependency]
     addTask newTask = do
@@ -34,10 +39,31 @@ taskServer pool = addTask
           runDb pool $ insertMany_ $ map (\x -> Dependency (toSqlKey x) taskKey) dependencyList
           fmap (map entityVal) $ runDb pool $ selectList [DependencyChild ==. taskKey] []
 
+    sort :: Handler [Int]
+    sort = do
+      depTuple <- loadDep pool
+      taskEntityList <- runDb pool $ selectList ([] :: [Filter DbTask]) []
+      return $ sortDep $ concatDep (map (fromIntegral . fromSqlKey . entityKey) taskEntityList) depTuple
 
 
+loadDep :: ConnectionPool -> Handler [(Int,Int)]
+loadDep pool =
+  fmap ( map (depAsTuple . entityVal)) $ runDb pool $ selectList ([] :: [Filter Dependency]) []
+  where
+    depAsTuple :: Dependency -> (Int, Int)
+    depAsTuple x = (fromIntegral $ fromSqlKey $ dependencyChild x, fromIntegral $ fromSqlKey $ dependencyParent x)
 
-loadAllTasks :: ConnectionPool -> Handler [DbTask]
-loadAllTasks pool =  do
-  allTasks <- runDb pool $ selectList [] []
-  return $ fmap entityVal allTasks
+concatDep :: [Int] -> [(Int, Int)] -> [(Int, [Int])]
+concatDep t d =
+  map (\x -> (x, map snd $ filter ((==) x . fst) d )) t
+
+sortDep :: [(Int, [Int])] -> [Int]
+sortDep [] = []
+sortDep xs = withoutChild <> sortDep (map removeAlreadyListed withParent)
+  where
+    withoutChild :: [Int]
+    withoutChild = map fst $ filter (null . snd) xs
+    withParent :: [(Int,[Int])]
+    withParent = filter (\(x,_) -> x `notElem` withoutChild) xs
+    removeAlreadyListed :: (Int, [Int]) -> (Int, [Int])
+    removeAlreadyListed (x, ys)= (x, filter (`elem` withoutChild) ys)
--- dash-server/test/TaskSpec.hs
+++ dash-server/test/TaskSpec.hs
@@ -8,6 +8,7 @@ import           Network.HTTP.Types.Header
 import           Network.HTTP.Types.Method
 import           Servant
 import           Server                     (initApp)
+import           Task                       (concatDep, sortDep)
 import           Test.Hspec
 import           Test.Hspec.Wai
 import           Test.Mockery.Directory
@@ -15,7 +16,7 @@ import           Test.Mockery.Directory
 spec :: Spec
 spec = do
 
-  with (withApp) $ do
+  with withApp $
 
     describe "/task" $ do
         it "add with dependencies ok" $ do
@@ -25,9 +26,25 @@ spec = do
             `shouldRespondWith` 200
           request methodGet "/task/add" jsonHeader (pack "{\"name\":\"a\",\"dependencies\":[1,2]}")
             `shouldRespondWith` "[{\"dependencyParent\":1,\"dependencyChild\":3},{\"dependencyParent\":2,\"dependencyChild\":3}]"
+          request methodGet "/task/add" jsonHeader (pack "{\"name\":\"a\",\"dependencies\":[1,3]}")
+            `shouldRespondWith` "[{\"dependencyParent\":1,\"dependencyChild\":4},{\"dependencyParent\":3,\"dependencyChild\":4}]"
+
         it "add with dependencies not ok" $
           request methodGet "/task/add" jsonHeader (pack "{\"name\":\"a\",\"dependencies\":[1]}")
             `shouldRespondWith` "unsatisfiedDeps: [1]" {matchStatus = 406}
+
+  describe "concatDep" $ do
+    it "on empty" $
+      concatDep [1,2] []  `shouldBe` [(1, []), (2, [])]
+    it "no dependencies for one" $
+      concatDep [1,2] [(2,1)] `shouldBe` [(1, []), (2, [1])]
+  describe "sortDep" $ do
+    it "empty dependencies" $
+      sortDep [(1,[]),(2,[])] `shouldBe` [1,2]
+    it "with dependencies" $
+      sortDep [(1,[]), (2,[1]), (3,[1,2]), (4,[1,3])] `shouldBe` [1,2,3,4]
+
+
 
 
 jsonHeader :: RequestHeaders
